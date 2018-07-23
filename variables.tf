0155edf90d0dc0dbceab7bde6e172a54697c6a7c
Kuzma Rezchenko <kuzma_rezchenko@epam.com>
2018-07-23 21:15:29 +0000

    remove extra variables with name load balancers

--- variables.tf
+++ variables.tf
@@ -4,15 +4,11 @@ variable "platform_name" {}
 
 variable "platform_default_subdomain" {}
 
-variable "platform_lb_name" {}
-
 variable "platform_lb_zone_id" {}
 
 variable "platform_lb_dns_name" {}
 
 variable "master_dns_name" {}
-
-variable "master_lb_name" {}
 
 variable "master_lb_zone_id" {}
 
