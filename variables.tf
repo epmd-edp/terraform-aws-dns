84e99dbe5e726ebce518e5ecd63ead0f10e0eaa9
Kuzma Rezchenko <kuzma_rezchenko@epam.com>
2018-07-23 21:14:02 +0000

    add zone_id and dns_name for load balancers

--- variables.tf
+++ variables.tf
@@ -4,11 +4,19 @@ variable "platform_name" {}
 
 variable "platform_default_subdomain" {}
 
-variable "platform_lb_arn" {}
+variable "platform_lb_name" {}
+
+variable "platform_lb_zone_id" {}
+
+variable "platform_lb_dns_name" {}
 
 variable "master_dns_name" {}
 
-variable "master_lb_arn" {}
+variable "master_lb_name" {}
+
+variable "master_lb_zone_id" {}
+
+variable "master_lb_dns_name" {}
 
 variable "tags" {
   type = "map"
