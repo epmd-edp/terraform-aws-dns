7dab420154a75a4808958a299babc2aad2911d6f
Kuzma Rezchenko <kuzma_rezchenko@epam.com>
2018-07-23 21:16:19 +0000

    replace data source by variables

--- route53.tf
+++ route53.tf
@@ -1,11 +1,3 @@
-data "aws_elb" "master_lb" {
-  name = "${var.master_lb_arn}"
-}
-
-data "aws_elb" "platform_lb" {
-  name = "${var.platform_lb_arn}"
-}
-
 resource "aws_route53_zone" "private" {
   name   = "${var.platform_default_subdomain}"
   vpc_id = "${var.platform_vpc_id}"
@@ -18,8 +10,8 @@ resource "aws_route53_record" "platform" {
   type    = "A"
 
   alias {
-    name                   = "${data.aws_elb.platform_lb.dns_name}"
-    zone_id                = "${data.aws_elb.platform_lb.zone_id}"
+    name                   = "${var.platform_lb_dns_name}"
+    zone_id                = "${var.platform_lb_zone_id}"
     evaluate_target_health = false
   }
 }
@@ -30,8 +22,8 @@ resource "aws_route53_record" "master" {
   type    = "A"
 
   alias {
-    name                   = "${data.aws_elb.master_lb.dns_name}"
-    zone_id                = "${data.aws_elb.master_lb.zone_id}"
+    name                   = "${var.master_lb_dns_name}"
+    zone_id                = "${var.master_lb_zone_id}"
     evaluate_target_health = false
   }
 }
