9df87da0c9be761bd76d6295e392d2001bd6b104
Olesia Vovk <olesia_ivanenko@epam.com>
2018-07-24 01:38:40 +0000

    Add master and platform lb name variables

--- variables.tf
+++ variables.tf
@@ -4,11 +4,15 @@ variable "platform_name" {}
 
 variable "platform_default_subdomain" {}
 
+variable "platform_lb_name" {}
+
 variable "platform_lb_zone_id" {}
 
 variable "platform_lb_dns_name" {}
 
 variable "master_dns_name" {}
+
+variable "master_lb_name" {}
 
 variable "master_lb_zone_id" {}
 
